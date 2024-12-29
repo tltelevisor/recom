57b738b3d9e4ab7c523a8342c11f273b72df6b7b
Max <tltelevisor@gmail.com>
2024-12-29 20:39:26 +0300

    dotenv

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import logging, os
+
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
 logging.basicConfig(
     level=logging.INFO, filename='py.log',
     format="%(asctime)s - [%(levelname)s] %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
@@ -6,14 +13,14 @@ logging.basicConfig(
 # logging.getLogger("httpx").setLevel(logging.WARNING) 
 logger = logging.getLogger(__name__)
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-PHONE_NUMBER= os.getenv("PHONE_NUMBER")
-NAME = os.getenv("NAME")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+PHONE_NUMBER= os.environ.get("PHONE_NUMBER")
+NAME = os.environ.get("NAME")
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.environ.get("API_KEY")
 OPENAI_MODEL = 'gpt-4o'
 
 from os.path import abspath, dirname, join
